6b8736b684f879df9a327ab2ab0530887a5fa313
ACMerriman <acmerriman94@gmail.com>
2017-09-26 18:47:24 -0400

    first commit

---
+++ js/source.js
@@ -0,0 +1,178 @@
+var g_data;
+
+window.onload = function()
+{
+	$.ajax({
+			url: 'http://localhost:8081/data',						
+			success: function(data) {
+				//console.log('success');
+				//console.log(data);
+				g_data = data;
+				main(data);
+			},
+			error: function(data) {
+				console.log('error');
+				console.log(data);
+				
+				// two arguments: the id of the Timeline container (no '#')
+				// and the JSON object or an instance of TL.TimelineConfig created from
+				// a suitable JSON object
+				//window.timeline = new TL.Timeline('timeline-embed', 'marktwain_test.json');
+			}
+		});
+};
+
+function main()
+{
+	var matrix = g_data.matrix
+	
+	var table = document.getElementById('scoreTable');
+	var htmlstring = "";
+	
+	for(var i = -1; i <= g_data.maxpts; i++)
+	{
+		htmlstring += "<tr>";
+		for(var j = -1; j <= g_data.maxpts; j++)
+		{
+			if(i == -1)
+			{
+				if (j < 0)
+				{
+					htmlstring += "<th></th>";
+				}
+				else
+				{
+					htmlstring += "<th id='colHeader_" + j + "'>" + j + "</th>";
+				}
+			}
+			else
+			{
+				//coloring black squares
+				if(j < i - 1)
+				{
+					htmlstring += "<td class='black'></td>";
+				}
+				//adding number
+				else if (j == i-1)
+				{
+					htmlstring += "<th id='rowHeader_" + i + "' class='black'>" + i + "</th>";
+				}
+				//color in green squares
+				else if (matrix[i][j] > 0)
+				{
+					htmlstring += "<td id='cell_" + i + "-" + j + "' class='green'><a href='https://www.pro-football-reference.com/boxscores/game_scores_find.cgi?pts_win=" + j + "&pts_lose=" + i +"'>&nbsp</a></td>";
+					//var alpha = 0.9 * matrix[i][j] / g_data.maxcount + 0.1;
+					//htmlstring += "<td id='cell_" + i + "-" + j + "' class='green' style='background-color:rgba(0,128,0," + alpha + ")'><a href='https://www.pro-football-reference.com/boxscores/game_scores_find.cgi?pts_win=" + j + "&pts_lose=" + i +"'>&nbsp</a></td>";
+				}
+				//fill in empty squares
+				else
+				{
+					//color black squares for impossible scores along 1 point line
+					if( i == 1)
+					{
+						switch (j)
+						{
+							case 1:
+							case 2: 
+							case 3: 
+							case 4:
+							case 5:
+							case 7: 
+								htmlstring += "<td class='black'></td>";
+								break;
+							default:
+								htmlstring += "<td id='cell_" + i + "-" + j + "' class='blank'></td>";
+								break;
+								
+						}
+					}
+					else
+					{
+						htmlstring += "<td id='cell_" + i + "-" + j + "' class='blank'></td>";
+					}
+				}
+			}
+		}
+		htmlstring += "</tr>";
+	}
+	table.innerHTML = htmlstring;
+	
+	for(var i = 0; i <= g_data.maxpts; i++)
+	{
+		for(var j = 0; j <= g_data.maxpts; j++)
+		{
+			var cell = document.getElementById("cell_" + i + "-" + j);
+			if(cell)
+			{
+				cell.addEventListener('mouseover', mouseOverDelegate(i, j));
+				cell.addEventListener('mouseout', mouseOffDelegate(i, j));
+			}
+		}
+	}
+	
+	//onmouseover='mouseOver(" + i + "," + j + ");' onmouseout='mouseOff(" + i + "," + j + ");'
+}
+function mouseOver(i, j)
+{
+	for(var k = 0; k <= g_data.maxpts; k++)
+	{
+		var cell = document.getElementById("cell_" + i + "-" + k);
+		if(cell && k != j)
+		{
+			if(cell.classList.contains('green'))
+			{
+				cell.style = "background-color:#008800;";
+			}
+			else
+			{
+				cell.style = "background-color:#EEEEEE;";
+			}
+		}
+		var cell = document.getElementById("cell_" + k + "-" + j);
+		if(cell && k != i)
+		{
+			if(cell.classList.contains('green'))
+			{
+				cell.style = "background-color:#008800;";
+			}
+			else
+			{
+				cell.style = "background-color:#EEEEEE;";
+			}
+		}
+	}
+	var colHeader = document.getElementById("colHeader_" + j);
+	colHeader.style = "background-color:yellow;font-size:12px;";
+	var rowHeader = document.getElementById("rowHeader_" + i);
+	rowHeader.style = "background-color:yellow;font-size:12px;color:black";
+}
+function mouseOff(i, j)
+{
+	for(var k = 0; k <= g_data.maxpts; k++)
+	{
+		var cell = document.getElementById("cell_" + i + "-" + k);
+		if(cell && k != j)
+		{
+			cell.style = "";
+		}
+		var cell = document.getElementById("cell_" + k + "-" + j);
+		if(cell && k != i)
+		{
+			cell.style = "";
+		}
+	}
+	var colHeader = document.getElementById("colHeader_" + j);
+	colHeader.style = "";
+	var rowHeader = document.getElementById("rowHeader_" + i);
+	rowHeader.style = "";
+}
+function mouseOverDelegate(i, j) {
+  return function(){
+      mouseOver(i, j)
+  }
+}
+function mouseOffDelegate(i, j) {
+  return function(){
+      mouseOff(i, j)
+  }
+}
---
+++ server.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var app = express();
+var path = require("path");
+
+app.use(express.static(__dirname + '/'));
+
+url = 'https://www.pro-football-reference.com/boxscores/game-scores.htm';
+var json = [];
+var matrix = [];
+var maxpts = 0;
+var maxcount = 0;
+
+function updateData()
+{
+	request(url, function(error, response, html)
+	{
+		if(!error)
+		{
+			console.log("fetching data");
+			var newjson = [];
+			var newmatrix = [];
+			html = html.substr(html.indexOf('<tr >'), html.length);
+			var PTS_WIN_IDENTIFIER = 'data-stat="pts_win" >';
+			var PTS_LOSE_IDENTIFIER = 'data-stat="pts_lose" >';
+			var COUNTER_IDENTIFIER = 'data-stat="counter" >';
+			while(html.indexOf('<tr >') >= 0)
+			{
+				var object = {};
+				html = html.substr(html.indexOf(PTS_WIN_IDENTIFIER) + PTS_WIN_IDENTIFIER.length, html.length);
+				object.ptsWin = parseInt(html.substr(0, html.indexOf("</td>")));
+				html = html.substr(html.indexOf(PTS_LOSE_IDENTIFIER) + PTS_LOSE_IDENTIFIER.length, html.length);
+				object.ptsLose = parseInt(html.substr(0, html.indexOf("</td>")));
+				html = html.substr(html.indexOf(COUNTER_IDENTIFIER) + COUNTER_IDENTIFIER.length, html.length);
+				object.count = parseInt(html.substr(0, html.indexOf("</td>")));
+				html = html.substr(html.indexOf('<tr >'), html.length);
+				newjson.push(object);
+			}
+			for(var i = 0; i < newjson.length; i++)
+			{
+				if(newjson[i].ptsWin > maxpts)
+				{
+					maxpts = newjson[i].ptsWin;
+				}
+				if(newjson[i].count > maxcount)
+				{
+					maxcount = newjson[i].count;
+				}
+			}
+			for (var i = 0; i <= maxpts; i++)
+			{
+				newmatrix[i] = [];
+				for(var j = 0; j <= maxpts; j++)
+				{
+					newmatrix[i][j] = 0;
+				}
+			}
+			for(var i = 0; i < newjson.length; i++)
+			{
+				newmatrix[newjson[i].ptsLose][newjson[i].ptsWin] = newjson[i].count;
+			}
+			json = newjson;
+			matrix = newmatrix;
+			var date = new Date();
+			console.log("done " + date.toUTCString());
+		}
+		else
+		{
+			console.log("There was an error");
+		}
+	});
+}
+
+updateData();
+
+setInterval(updateData, 1000 * 60 * 60);
+	
+app.get('/data', function(req, res)
+{
+	var data = {
+		matrix: matrix,
+		maxpts: maxpts,
+		maxcount: maxcount
+	};
+	//console.log(data);
+	res.json(data);
+});
+
+app.get('/', function(req, res)
+{
+	res.sendFile(path.join(__dirname+"/view/index.html"));
+});
+
+app.listen(8081);
