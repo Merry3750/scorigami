68cb77b23304a77a5e991faefdbd48664731adc7
ACMerriman <acmerriman94@gmail.com>
2017-09-28 19:31:26 -0400

    testing database part 3

--- pgtest.js
+++ pgtest.js
@@ -7,7 +7,7 @@ const client = new Client({
 
 client.connect();
 
-client.query('INSERT INTO test (id INTEGER, text TEXT) VALIES (0, "hello, world");', (err, res) => {
+client.query('INSERT INTO test (id, text) VALIES (0, "hello, world");', (err, res) => {
 	if (err) throw err;
 	client.query('SELECT * FROM test;', (err, res) => {
 	  if (err) throw err;
