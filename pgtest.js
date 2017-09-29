d8778cba72c601ad37e7c6d2d24bdb3bd9d39d71
ACMerriman <acmerriman94@gmail.com>
2017-09-28 20:01:21 -0400

    It didn't work but hopfully it will this time

--- pgtest.js
+++ pgtest.js
@@ -1,5 +1,9 @@
 const { Client } = require('pg');
-var dbinfo = require('./connection_string');
+var dbinfo = require('./connection_string'); || "";
+
+console.log(process.env.DATABASE_URL);
+
+var connectionString = process.env.DATABASE_URL || dbinfo.URI
 
 const client = new Client({
   connectionString: dbinfo.URI || process.env.DATABASE_URL,
@@ -11,7 +15,7 @@ client.connect();
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
-    console.log(JSON.stringify(row));
+    //console.log(JSON.stringify(row));
   }
   client.end();
 });
