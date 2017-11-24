164f2817da7a12ee1a735482e701dc55c9a58446
Your Name <you@example.com>
2017-11-24 16:08:31 -0500

    removed renderpage from server.js since it was unused

--- js/server.js
+++ js/server.js
@@ -187,7 +187,6 @@ function updateData()
 															else
 															{
 																console.log("There was an error updating data: 4");
-																console.log(err4);
 																getData();
 															}
 														});
@@ -285,125 +284,6 @@ function getData()
 			throw err;
 		}
 		//renderPage();
-	});
-}
-
-//Renders page, unused as of 2017/10/30
-function renderPage()
-{
-	fs.readFile(path.join(__dirname+"/view/indexTemplate.html"), 'utf8', function(err, data)
-	{
-		// SECTION - Building the table
-		var htmlstring = "\n";
-
-		//cycle through all elements in the table (maxpts will always be the length and width of the matrix)
-		//start at -1 so labels can be added
-		for(var i = -1; i <= maxpts; i++)
-		{
-			htmlstring += "\t\t\t<tr id='row_" + i + "'>\n";
-			for(var j = 0; j <= maxpts + 1; j++)
-			{
-				//if i==-1, we are in the label row
-				if(i == -1)
-				{
-					//do not label the top right cell, since the left column is all labels
-					if (j > maxpts)
-					{
-						htmlstring += "\t\t\t\t<th></th>\n";
-					}
-					//adding column lables
-					else 
-					{
-						htmlstring += "\t\t\t\t<th id='colHeader_" + j + "'>" + j + "</th>\n";
-					}
-				}
-				else
-				{
-					//coloring black squares
-					if(j < i - 1)
-					{
-						htmlstring += "\t\t\t\t<td class='black'></td>\n";
-					}
-					//adding row label
-					else if (j == i - 1)
-					{
-						htmlstring += "\t\t\t\t<th id='specialHeader_" + i + "' class='black'></th>\n";
-					}
-					//adding row label
-					else if (j == maxpts + 1)
-					{
-						htmlstring += "\t\t\t\t<th id='rowHeader_" + i + "'>" + i + "</th>\n";
-					}
-					//color in green squares
-					else if (matrix[i][j] > 0)
-					{
-						htmlstring += "\t\t\t\t<td id='cell_" + i + "-" + j + "' class='green'><a href='https://www.pro-football-reference.com/boxscores/game_scores_find.cgi?pts_win=" + j + "&pts_lose=" + i +"'><div id='hover_" + i + "-" + j + "' class='hover'><div id='count_" + i + "-" + j + "' class='count'>" + matrix[i][j] + "</div></div></a></td>\n";
-					}
-					//fill in empty squares
-					else
-					{
-						//color black squares for impossible scores along 1 point line
-						//NOTE: we can do this after coloring in the green squares since these squares will never be green
-						if( i == 1)
-						{
-							switch (j)
-							{
-								case 1:
-								case 2: 
-								case 3: 
-								case 4:
-								case 5:
-								case 7: 
-									htmlstring += "\t\t\t\t<td class='black'></td>\n";
-									break;
-								default:
-									htmlstring += "\t\t\t\t<td id='cell_" + i + "-" + j + "' class='blank'><div id='hover_" + i + "-" + j + "' class='hover'></div></td>\n";
-									break;
-									
-							}
-						}
-						//color 0,1 square black since that is also impossible
-						//NOTE: we can do this after coloring in the green squares since this square will never be green
-						else if (i == 0 && j == 1)
-						{
-							htmlstring += "\t\t\t\t<td class='black'></td>";
-						}
-						else
-						{
-							htmlstring += "\t\t\t\t<td id='cell_" + i + "-" + j + "' class='blank'><div id='hover_" + i + "-" + j + "' class='hover'></div></td>\n";
-						}
-					}
-				}
-			}
-			htmlstring += "\t\t\t</tr>\n";
-		}
-		htmlstring += "\t\t";
-		data = data.replace("{{scoreTable}}", htmlstring);
-		// END SECTION - Building the table
-		
-		//SECTION - Building the spectrum	
-		//populate hue spectrum (because doing this manually would be tedious and difficult to make changes to)
-		htmlstring = "\n";
-		
-		var MAX_HUE = 240.0;
-		
-		var num = 600 / Math.log(MAX_HUE + 2);
-		
-		for(var i = 0; i <= MAX_HUE; i++)
-		{
-			var width = (Math.log(MAX_HUE + 2 - i) - Math.log(MAX_HUE + 1 - i)) * num;
-			htmlstring += "\t\t\t\t\t\t\t<span id='hue_" + i + "' class='hueColor' style='background-color:hsl(" + (MAX_HUE - i) + ",50%,50%);width:" + width + "px'></span>\n";
-		}
-		htmlstring += "\t\t\t\t\t\t"
-		data = data.replace("{{hueSpectrumColors}}", htmlstring);
-		
-		data = data.replace("{{hueSpectrumLabelMaxCount}}", maxcount);
-		//END SECTION - Building the spectrum	
-		
-		fs.writeFile(path.join(__dirname+"/view/indexRendered.html"), data, 'utf8', function()
-		{
-		});
-		
 	});
 }
 
