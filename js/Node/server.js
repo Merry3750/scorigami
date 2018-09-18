6da5f38cfa93a5c1ff2aeb3f504427ff6b9cff23
ACMerriman <ACMerriman@users.noreply.github.com>
2018-09-18 10:09:31 -0400

    removed extra / when redirecting

--- js/Node/server.js
+++ js/Node/server.js
@@ -14,7 +14,7 @@ app.use(function forceLiveDomain(req, res, next) {
   // Don't allow user to hit Heroku now that we have a domain
   var host = req.get('Host');
   if (host === 'scorigami.herokuapp.com') {
-    return res.redirect(301, 'https://nflscorigami.com/' + req.originalUrl);
+    return res.redirect(301, 'https://nflscorigami.com' + req.originalUrl);
   }
   return next();
 });
